75b78b029f881f7770cbf2418211ed6d5e80f74a
jaderohunmu <70451627+jaderohunmu@users.noreply.github.com>
2020-09-17 12:23:54 -0700

    fun jquery

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$('.meal2_grp button').hover(function() {
+    $('.meal2').css('background-image','url(./images/BackgroundProject1.jpg)');
+  })
+
+$('.meal2_grp button').mouseout(function() {
+    $('.meal2').css('background-image','url(./images/background-project-1@1x.jpg)');
+  })
